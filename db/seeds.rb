468fcf443569a54408262494d8a0fb18b3e0fa06
catalina baron <catalinatbaron@gmail.com>
2019-02-17 18:31:47 +0200

    seeds finnally done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,62 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts ' Destroying all the shit'
+
+Review.destroy_all
+Booking.destroy_all
+
+Creature.destroy_all
+User.destroy_all
+
+
+puts 'completed'
+
+
+
+puts 'Building some users'
+
+user1 = User.create!(name:'Kurt Cobain', email: 'kurco@gmail.com', password:'rocknroll')
+user2 = User.create!(name:'Sid Barret', email: 'sidbarret@gmail.com', password:'rocknroll')
+user3 = User.create!(name:'Amy Winehouse', email: 'amywinehouse@gmail.com', password:'rocknroll')
+user4 = User.create!(name:'Rick Sanchez', email: 'rickisdope@gmail.com', password:'rocknroll')
+user5 = User.create!(name:'Morty Smith ', email: 'msmith@gmail.com', password:'rocknroll')
+
+puts 'Making some creatures'
+creature1 = Creature.create!(name:'Unicorn', super_power:'I believe I can fly', price:'100', user_id: user2.id)
+creature2 = Creature.create!(name:'Flying Camel', super_power:"I'll take you to your destiny", price:'200', user_id: user3.id)
+creature3 = Creature.create!(name:'Dragon', super_power:'Fire', price:'150',user_id: user3.id)
+creature4 = Creature.create!(name:'Elf', super_power:'I believe I can fly', price:'100',user_id: user3.id)
+creature5 = Creature.create!(name:'Fairy', super_power:'I can make your dreams come true', price:'120',user_id: user3.id)
+
+puts 'Nice bookings '
+booking1 = Booking.create!(start_time: DateTime.new(2018,2,3.5), end_time: DateTime.new(2018,3,4), user_id: user1.id, creature_id: creature1.id)
+booking1 = Booking.create!(start_time: DateTime.new(2018,2,3.5), end_time: DateTime.new(2018,3,4), user_id: user2.id, creature_id: creature2.id)
+booking1 = Booking.create!(start_time: DateTime.new(2018,2,3.5), end_time: DateTime.new(2018,3,4), user_id: user3.id, creature_id: creature3.id)
+booking1 = Booking.create!(start_time: DateTime.new(2018,2,3.5), end_time: DateTime.new(2018,3,4), user_id: user4.id, creature_id: creature4.id)
+booking1 = Booking.create!(start_time: DateTime.new(2018,2,3.5), end_time: DateTime.new(2018,3,4), user_id: user5.id, creature_id: creature5.id)
+booking1 = Booking.create!(start_time: DateTime.new(2010,2,3.5), end_time: DateTime.new(2018,3,4), user_id: user1.id, creature_id: creature2.id)
+
+
+puts 'Amy Winehouse loves all kinds of creatures!'
+booking6 = Booking.create!(start_time: DateTime.new(2018,2,3.5), end_time: DateTime.new(2018,3,4), user_id: user3.id, creature_id: creature1.id)
+booking7 = Booking.create!(start_time: DateTime.new(2018,2,3.5), end_time: DateTime.new(2018,3,4), user_id: user3.id, creature_id: creature2.id)
+booking8 = Booking.create!(start_time: DateTime.new(2018,2,3.5), end_time: DateTime.new(2018,3,4), user_id: user3.id, creature_id: creature3.id)
+booking9 = Booking.create!(start_time: DateTime.new(2018,2,3.5), end_time: DateTime.new(2018,3,4), user_id: user3.id, creature_id: creature4.id)
+
+
+
+puts 'Fucking good reviews'
+
+puts 'Yeah'
+review1 = Review.create!(description: 'That unicorn really wanna make me kill myself', rating: 1)
+booking6.update(review_id: review1.id)
+review2 = Review.create!(description: 'Amy likes everything', rating: 10)
+booking7.update(review_id: review2.id)
+review3 = Review.create!(description: 'that flying camel is killing it ', rating: 10)
+booking8.update(review_id: review3.id)
+review4 = Review.create!(description: 'blablabla', rating: 1)
+booking9.update(review_id: review4.id)
+
+
+puts 'the end!'
