d9fcf1561c749c6d80b0aad9bff2fa442b43b603
Albrecht Bernstorff <albrecht.bernstorff@web.de>
2019-02-18 18:11:05 +0200

    added cloudinatry key and review rating validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,4 @@
 class Review < ApplicationRecord
   validates :description, presence: true
-  validates :rating, presence: true
+  validates :rating, presence: true, :inclusion => { :in => 0..5 }
 end
