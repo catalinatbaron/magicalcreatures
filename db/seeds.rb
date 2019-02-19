15bf7d7b39b3c72df5bb3748f4c46246465b91bc
Rony Hirsch <ynor6@outlook.com>
2019-02-19 11:27:00 +0200

    added pictures to the seeds, created add image in the new creature form

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,10 +29,10 @@ user5 = User.create!(name:'Morty Smith ', email: 'msmith@gmail.com', password:'r
 
 puts 'Making some creatures'
 creature1 = Creature.create!(name:'Unicorn', super_power:'I believe I can fly', price:'100', user_id: user2.id, remote_photo_url: "https://i.pinimg.com/564x/c1/65/f4/c165f409a2846a67d7e023953f1c614a.jpg?b=t")
-creature2 = Creature.create!(name:'Flying Camel', super_power:"I'll take you to your destiny", price:'200', user_id: user3.id)
-creature3 = Creature.create!(name:'Dragon', super_power:'Fire', price:'150',user_id: user3.id)
-creature4 = Creature.create!(name:'Elf', super_power:'I believe I can fly', price:'100',user_id: user3.id)
-creature5 = Creature.create!(name:'Fairy', super_power:'I can make your dreams come true', price:'120',user_id: user3.id)
+creature2 = Creature.create!(name:'Flying Camel', super_power:"I'll take you to your destiny", price:'200', user_id: user3.id, remote_photo_url: "https://i.pinimg.com/564x/74/24/24/742424c9e132a0be41beaad4dda34132.jpg?b=t")
+creature3 = Creature.create!(name:'Dragon', super_power:'Fire', price:'150',user_id: user3.id, remote_photo_url: "https://i.pinimg.com/564x/75/71/96/757196a3684622065bb06090129cc9a5.jpg?b=t")
+creature4 = Creature.create!(name:'Elf', super_power:'I believe I can fly', price:'100',user_id: user3.id, remote_photo_url: "https://i.pinimg.com/564x/db/b1/cf/dbb1cf874ebb97338b87ae74d1cc0492.jpg?b=t")
+creature5 = Creature.create!(name:'Fairy', super_power:'I can make your dreams come true', price:'120',user_id: user3.id, remote_photo_url: "https://i.pinimg.com/564x/a2/06/88/a206889f40b72ff17a688c95a17215d5.jpg")
 
 puts 'Nice bookings '
 booking1 = Booking.create!(start_time: DateTime.new(2018,2,3.5), end_time: DateTime.new(2018,3,4), user_id: user1.id, creature_id: creature1.id)
@@ -56,11 +56,11 @@ puts 'Fucking good reviews'
 puts 'Yeah'
 review1 = Review.create!(description: 'That unicorn really wanna make me kill myself', rating: 1)
 booking6.update(review_id: review1.id)
-review2 = Review.create!(description: 'Amy likes everything', rating: 10)
+review2 = Review.create!(description: 'Amy likes everything', rating: 3)
 booking7.update(review_id: review2.id)
-review3 = Review.create!(description: 'that flying camel is killing it ', rating: 10)
+review3 = Review.create!(description: 'that flying camel is killing it ', rating: 4)
 booking8.update(review_id: review3.id)
-review4 = Review.create!(description: 'blablabla', rating: 1)
+review4 = Review.create!(description: 'blablabla', rating: 2)
 booking9.update(review_id: review4.id)
 
 
